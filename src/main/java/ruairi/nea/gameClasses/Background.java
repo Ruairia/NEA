d4c8c41be49e7b930a9fd0c6ad24908aec822e27
ruairi <ruairianthony@outlook.com>
2025-12-18 15:32:05 +0000

    Added Cave Background

--- src/main/java/ruairi/nea/gameClasses/Background.java
+++ src/main/java/ruairi/nea/gameClasses/Background.java
@@ -4,37 +4,88 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import ruairi.nea.applicationClasses.Main;
 
+import java.util.ArrayList;
+
 public class Background {
-    final float BACKGROUND_STRETCH_X = 2.5f;
-    final float BACKGROUND_STRETCH_Y = 1.7f;
 
-    private final Texture backgroundFar;
-    private final Texture backgroundMid;
-    private final Texture backgroundNear;
+    private static class BackgroundLayer{
+        Texture texture;
+        float parallax;
+        boolean inFrontOfCamera = false;
 
-    public Background(int level) {
-        switch (level){
-            default:
-                backgroundFar = new Texture("assets/background_far.png");
-                backgroundMid = new Texture("assets/background_mid.png");
-                backgroundNear = new Texture("assets/background_near.png");
+        BackgroundLayer(Texture texture, float parallax,boolean inFrontOfCamera) {
+            this.texture = texture;
+            this.parallax = parallax;
+            this.inFrontOfCamera = inFrontOfCamera;
         }
     }
 
-    public void drawBackground(Camera camera, Main game){
-        drawLayer(camera, game, backgroundFar, 0.05f);
-        drawLayer(camera, game, backgroundMid, 0.1f);
-        drawLayer(camera, game, backgroundNear, 0.2f);
+    private final ArrayList<BackgroundLayer> layers = new ArrayList<>();
+
+    final float OAK_BACKGROUND_STRETCH_X = 2.5f;
+    final float OAK_BACKGROUND_STRETCH_Y = 1.7f;
+    final float CAVE_BACKGROUND_STRETCH_X = 2.5f;
+    final float CAVE_BACKGROUND_STRETCH_Y = 1.7f;
+
+    final String backgroundType;
+
+    public Background(int level) {
+
+        switch (level){
+            case 3,4:
+                addLayer(("assets/CaveBackgroundFar.png"),0.05f, false);
+                addLayer(("assets/CaveBackgroundMid.png"),0.1f, false);
+                addLayer(("assets/CaveBackgroundNearA.png"),0.2f, false);
+                backgroundType = "Cave";
+                break;
+            default:
+                addLayer(("assets/OakBackgroundFar.png"),0.05f, false);
+                addLayer(("assets/OakBackgroundMid.png"),0.1f, false);
+                addLayer(("assets/OakBackgroundNear.png"),0.2f, false);
+                backgroundType = "Oak";
+        }
     }
 
-    private void drawLayer(Camera camera, Main game, Texture texture, float parallaxCoefficient) {
+    private void addLayer(String texturePath, float parallax, boolean inFrontOfCamera) {
+        layers.add(new BackgroundLayer(new Texture(texturePath), parallax, inFrontOfCamera));
+    }
+
+    public void drawBackgroundBehindCamera(Camera camera, Main game){
+        for (BackgroundLayer layer : layers) {
+            if (layer.inFrontOfCamera) continue;
+            drawLayer(camera, game, layer, layer.parallax);
+        }
+    }
+
+    public void drawBackgroundInFrontOfCamera(Camera camera, Main game){
+        for (BackgroundLayer layer : layers) {
+            if (!layer.inFrontOfCamera) continue;
+            drawLayer(camera, game, layer, layer.parallax);
+        }
+    }
+
+    private void drawLayer(Camera camera, Main game, BackgroundLayer layer, float parallaxCoefficient) {
+
+        float backgroundStretchX;
+        float backgroundStretchY;
+
+        switch (backgroundType){
+            case "Cave":
+                backgroundStretchX = CAVE_BACKGROUND_STRETCH_X;
+                backgroundStretchY = CAVE_BACKGROUND_STRETCH_Y;
+                break;
+            default:
+                backgroundStretchX = OAK_BACKGROUND_STRETCH_X;
+                backgroundStretchY = OAK_BACKGROUND_STRETCH_Y;
+        }
+
         float parallaxOffsetX = camera.position.x * parallaxCoefficient;
         float parallaxOffsetY = camera.position.y * parallaxCoefficient;
 
         float drawY = camera.position.y - camera.viewportHeight/2 - parallaxOffsetY;
-        float drawHeight = camera.viewportHeight * BACKGROUND_STRETCH_Y;
+        float drawHeight = camera.viewportHeight * backgroundStretchY;
 
-        int textureWidth = (int) (texture.getWidth()* BACKGROUND_STRETCH_X);
+        int textureWidth = (int) (layer.texture.getWidth()* backgroundStretchX);
 
 
         float baseX = camera.position.x - camera.viewportWidth/2 - parallaxOffsetX;
@@ -43,10 +94,12 @@ public class Background {
         float x = baseX - absMod(parallaxOffsetX, textureWidth);
 
         while (x < camera.position.x + camera.viewportWidth/2) {
-            game.batch.draw(texture, x, drawY, textureWidth, drawHeight);
+            game.batch.draw(layer.texture, x, drawY, textureWidth, drawHeight);
             x += textureWidth;
         }
     }
+
+
 
 
     private float absMod(float a, float b) {
@@ -54,8 +107,8 @@ public class Background {
     }
 
     public void dispose() {
-        backgroundFar.dispose();
-        backgroundMid.dispose();
-        backgroundNear.dispose();
+        for (BackgroundLayer layer : layers) {
+            layer.texture.dispose();
+        }
     }
 }
